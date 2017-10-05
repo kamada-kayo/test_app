48056910066f13b2ba1e2b18248d8755f4f93509
kayo kamada <kayo.kamada0@gmail.com>
2017-10-05 19:35:55 +0900

    lesson19

--- functions.php
+++ functions.php
@@ -1,8 +1,34 @@
 <?php
 require('connection.php');
+session_start();
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
+
+function setToken() {
+  $token = sha1(uniqid(mt_rand(), true));
+  $_SESSION['token'] = $token;
+}
+
+function checkToken($data) {
+  if (empty($_SESSION['token']) || ($_SESSION['token'] != $data)){
+    $_SESSION['err'] = '不正な操作です';
+    header('location: '.$SERVER_['HTTP_REFERER'].'');
+    exit;
+  }
+  return true;
+}
+
+function unsetSession() {
+  if(!empty($_SESSION['err'])) $_SESSION['err'] = '';
+}
+
 
 function create($data) {
+  if(checkToken($data['token'])){
   insertDb($data['todo']);
+  }
 }
 
 function index() {
@@ -10,7 +36,9 @@ function index() {
 }
 
 function update($data) {
+  if(checkToken($data['token'])){
   updateDb($data['id'], $data['todo']);
+  }
 }
 
 function detail($id) {
--- new.php
+++ new.php
@@ -1,3 +1,8 @@
+<?php
+require_once('functions.php');
+setToken();
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,13 +10,17 @@
   <title>新規作成</title>
 </head>
 <body>
+  <?php if(isset($_SEESION['err'])): ?>
+    <p><?php echo $_SESSION['err'] ?></p>
+  <?php endif; ?>
   <form action="store.php" method="post">
-    <input type="hidden" name="token" value="">
+    <input type="hidden" name="token" value="<?php echo $_SESSION['token']); ?>">
     <input type="text" name="todo">
     <input type="submit" value="作成">
   </form>
   <div>
     <a href="index.html">一覧へもどる</a>
   </div>
+  <?php unsetSession(); ?>
 </body>
 </html>
